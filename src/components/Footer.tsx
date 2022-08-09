76ce7c515f5969aef63dde2d87438f29f9a34e48
Lukas Bach <lukasbach@users.noreply.github.com>
2022-08-10 01:52:41 +0200

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,6 +5,7 @@ export const Footer: React.FC<{}> = props => {
     <footer>
       © 2020 Lukas Bach -{' '}
       <a href="/impress">Impress</a> -{' '}
+      <a href="/privacy">Privacy and Cookie Policy</a> -{' '}
       Square Pattern by <a href="http://www.heropatterns.com/" target="_blank">HeroPatterns</a>{' '}
       <a href="https://creativecommons.org/licenses/by/4.0/" target="_blank">CC BY 4.0</a>
     </footer>
